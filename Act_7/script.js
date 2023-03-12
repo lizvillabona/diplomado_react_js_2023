163db5d72a6630d58e4aa2f5d0a27ebec6b80f23
Liz Villabona <liz.villabona@gmail.com>
2023-03-12 17:34:54 -0500

    ACT_7

---
+++ Act_7/script.js
@@ -0,0 +1,7 @@
+document.write('Hola mundo, Escribiendo en JS')
+
+//alert mensaje sin necesidad de abrir consola
+//a= window.prompt("Inserte su nombre:")
+
+//document.write("Hola mundo! <br>");
+//document.write(`Hola ${a}, Bienvenido`);
---
+++ Act_7/script2.js
@@ -0,0 +1,13 @@
+document.write ("<h1> Lizeth Villabona Niño</h1>")
+    
+document.write("<h2>Descripcion:</h2>")
+document.write("Ingeniera electronica graduada en el año 2015 con experiencia en automatizacion industrial y equipos biomedicos")
+
+document.write("<h2>Herramientas trabajadas</h2>")
+
+    document.write("<div>")
+        document.write("<ul>")
+        document.write("<li>HTML </li> ") 
+        document.write("<li>CSS </li> ") 
+        document.write("<li>PHP </li> ") 
+               
